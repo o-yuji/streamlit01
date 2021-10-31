0ece9e5777e897c598caab3b821af3f35b6ab588
o-yuji <py_ootsuka@yahoo.co.jp>
2021-10-31 23:44:36 +0900

    04

--- wage_streamlit.py
+++ wage_streamlit.py
@@ -6,13 +6,13 @@ import plotly.express as px
 
 st.title("賃金データダッシュボード")
 
-df_jp_ind = pd.read_csv("./csv_data\雇用_医療福祉_一人当たり賃金_全国_全産業.csv",encoding="shift-jis")
-df_jp_category =pd.read_csv("./csv_data\雇用_医療福祉_一人当たり賃金_全国_大分類.csv",encoding="shift-jis")
-df_pref_ind = pd.read_csv("./csv_data\雇用_医療福祉_一人当たり賃金_都道府県_全産業.csv",encoding="shift-jis")
+df_jp_ind = pd.read_csv("./csv_data/雇用_医療福祉_一人当たり賃金_全国_全産業.csv",encoding="shift-jis")
+df_jp_category =pd.read_csv("./csv_data/雇用_医療福祉_一人当たり賃金_全国_大分類.csv",encoding="shift-jis")
+df_pref_ind = pd.read_csv("./csv_data/雇用_医療福祉_一人当たり賃金_都道府県_全産業.csv",encoding="shift-jis")
 
 # df_jp_category
 
-st.header("●2019年:平均賃金(一人)のヒートマップ")
+st.header("2019年:平均賃金(一人)のヒートマップ")
 
 jp_lat_lon = pd.read_csv("pref_lat_lon.csv",encoding="utf-8")
 jp_lat_lon = jp_lat_lon.rename(columns={"pref_name":"都道府県名"})
@@ -51,7 +51,7 @@ if show_df == True:
     st.write(df_pref_map)
 
 
-st.header("●集計年別の賃金(一人)の推移")
+st.header("集計年別の賃金(一人)の推移")
 
 df_ts_mean = df_jp_ind[df_jp_ind["年齢"] == "年齢計"]
 df_ts_mean = df_ts_mean.rename(columns={"一人当たり賃金（万円）":"全国_一人当たり賃金(万円)"})
@@ -73,7 +73,7 @@ df_mean_line = df_mean_line[["集計年","全国_一人当たり賃金(万円)",
 df_mean_line = df_mean_line.set_index("集計年")
 st.line_chart(df_mean_line)
 
-st.header("●年齢別、全国平均賃金(1人)")
+st.header("年齢別、全国平均賃金(1人)")
 
 df_mean_bubble = df_jp_ind[df_jp_ind["年齢"] != "年齢計"]
 # st.dataframe(df_mean_bubble,width=2000)
@@ -91,7 +91,7 @@ fig = px.scatter(df_mean_bubble,
 
 st.plotly_chart(fig)
 
-st.header("●産業別の賃金推移")
+st.header("産業別の賃金推移")
 
 year_list = df_jp_category["集計年"].unique()
 option_year = st.selectbox("集計年", year_list)
